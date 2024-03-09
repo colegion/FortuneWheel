59910844e42c82021473e3e08c2d74b402063352
Ahmet Hakan Hafif <59022934+colegion@users.noreply.github.com>
2024-03-09 19:57:29 +0300

    fix
    making sure of shuffling

--- Assets/Scripts/Utilities/DictionaryExtensions.cs
+++ Assets/Scripts/Utilities/DictionaryExtensions.cs
@@ -9,13 +9,27 @@ namespace Utilities
         public static void Shuffle<TKey, TValue>(this IDictionary<TKey, TValue> dictionary)
         {
             Random random = new Random();
-            List<TKey> keys = dictionary.Keys.Where(key => !key.Equals(CommonFields.ItemClass.Bomb)).ToList();
+            List<TKey> keys = new List<TKey>(dictionary.Keys);
             int n = keys.Count;
-            while (n > 0)
+            while (n > 1)
             {
                 n--;
                 int k = random.Next(n + 1);
                 (keys[k], keys[n]) = (keys[n], keys[k]);
+            }
+
+            // Construct a new dictionary with shuffled keys
+            Dictionary<TKey, TValue> shuffledDictionary = new Dictionary<TKey, TValue>();
+            foreach (var key in keys)
+            {
+                shuffledDictionary[key] = dictionary[key];
+            }
+
+            // Replace original dictionary with shuffled dictionary
+            dictionary.Clear();
+            foreach (var kvp in shuffledDictionary)
+            {
+                dictionary[kvp.Key] = kvp.Value;
             }
         }
 
